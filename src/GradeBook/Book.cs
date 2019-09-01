3345074e9a8df9812067a7d4c67bfa49f75e7d26
barbarastempien <barbara.stempien@gmail.com>
2019-09-01 18:51:17 +0200

    feat: Add logic for statistics calculation

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,6 +1,37 @@
 
+using System;
+using System.Collections.Generic;
+
 namespace GradeBook {
     class Book {
-        
+
+        public Book() {
+            grades = new List<double>();
+        }
+
+        public void AddGrade(double grade) {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach(var number in grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+        }
+
+        List<double> grades;
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,6 +7,11 @@ namespace GradeBook
         static void Main(string[] args)
         {
            var book = new Book();
+           book.AddGrade(10.2);
+           book.AddGrade(15.8);
+           book.AddGrade(5.0);
+
+           book.ShowStatistics();
         }
     }
 }
